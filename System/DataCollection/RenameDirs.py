877922682ddceadc05f96d1ba18afc507162a9d9
WuLC <liangchaowu5@gmail.com>
2017-12-01 10:37:35 +0800

    fix bug

--- System/DataCollection/RenameDirs.py
+++ System/DataCollection/RenameDirs.py
@@ -5,19 +5,24 @@
 
 import os
 import argparse
-
+import shutil
 
 parser = argparse.ArgumentParser(description='Sort Files in a directory')
-parser.add_argument('--dir', help = 'the directory that needs to be sorted')
+parser.add_argument('--src_dir', help = 'the directory of the source files')
+parser.add_argument('--des_dir', help = 'the directory of the destinate files')
 parser.add_argument('--start', help = 'the number used to start the naming of dirs')
 args = parser.parse_args()
-des_dir = args.dir
+src_dir = args.src_dir
+des_dir = args.des_dir
 count = int(args.start)
 
-if not os.path.exists(des_dir):
-    print('path {0} not exists'.format(des_dir))
+if not os.path.exists(src_dir):
+    print('path {0} not exists'.format(src_dir))
 
-files = sorted(os.listdir(des_dir))
+if not os.path.exists(des_dir):
+    os.makedirs(des_dir)
+
+files = sorted(map(lambda x:int(x), [name for name in os.listdir(src_dir) if name.isdigit()]))
 for i in range(len(files)):
-    os.rename(des_dir+files[i], des_dir+str(count))
+    shutil.move(src_dir+str(files[i]), des_dir+str(count))
     count += 1
